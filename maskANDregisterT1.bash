c72f96055c86aea572ee21708dcd4131703cee7b
davegrays <daves.grays@gmail.com>
2016-04-12 18:23:32 -0700

    Update maskANDregisterT1.bash

--- maskANDregisterT1.bash
+++ maskANDregisterT1.bash
@@ -13,6 +13,14 @@ sub=$2
 atl=`readlink -f $3`
 atl_brain=`readlink -f $4`
 
+#check for existence of c3d_affine_tool in path
+path2c3d=`which c3d_affine_tool`
+if [[ $path2c3d == "" ]]; then
+	echo "FATAL ERROR: c3d_affine_tool not found."
+	echo "Make sure convert3D is properly installed and added to your path before using this tool."
+	exit 1
+fi
+
 pushd $dir
 
 #use dof6_head to first get the brain masks
@@ -43,6 +51,9 @@ rm ${sub}_rot2native.mat ${sub}_rot2atl_mask.nii.gz templateMask.nii.gz
 ;&
 dof6_brain)
 flirt -v -dof 6 -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -in ${sub}_brain -ref ${atl_brain} -o ${sub}_brain_rot2atl -omat ${sub}_rot2atl.mat
+#convert FSL mat to ANTS compatible itk mat
+c3d_affine_tool -ref ${sub}_brain_rot2atl.nii.gz -src ${sub}_brain.nii.gz ${sub}_rot2atl.mat -fsl2ras -oitk ${sub}_T1rot2atl.txt
+#N.B. if you want to use the inverse, specify so in the antsApplyTransforms command (rather than creating the inverse in FSL and converting to itk, which works improperly)
 
 ;&
 final_warp)
@@ -50,7 +61,7 @@ final_warp)
 ANTS 3 -m  CC[${sub}_brain_rot2atl.nii.gz,${atl_brain}.nii.gz,1,5] -t SyN[0.25] -r Gauss[3,0] -o atl2T1rot -i 50x60x30 --use-Histogram-Matching  --number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000
 #apply warp
 antsApplyTransforms -d 3 -i ${atl_brain}.nii.gz -t atl2T1rotWarp.nii.gz atl2T1rotAffine.txt -r ${sub}_rot2atl.nii.gz -o atl2T1rot_deforemdImage.nii.gz
-## NOTE: antsApplyTransforms is deprecated in ANTS 2.0 and might not be available; if so, use WarpImageMultiTransform instead
+## NOTE: antsApplyTransforms is deprecated in ANTS versions > 2.0 and might not be available; if so, use WarpImageMultiTransform instead
 
 esac
 
